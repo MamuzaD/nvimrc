70f7dc0a1b0169cc93076e5c665ce5c5abdd6ea8
Daniel Mamuza <94477022+MamuzaD@users.noreply.github.com>
2026-01-05 07:53:27 -0800

    refactor config

--- lua/daniel/autocmds.lua
+++ lua/daniel/autocmds.lua
@@ -12,6 +12,64 @@ autocmd("TextYankPost", {
   end,
 })
 
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "checkhealth",
+    "dbout",
+    "gitsigns-blame",
+    "grug-far",
+    "help",
+    "lspinfo",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "oil",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        if vim.bo[event.buf].filetype == "oil" then
+          require("oil").close()
+        else
+          vim.cmd("close")
+        end
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
+  end,
+})
+
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = augroup("auto_create_dir"),
@@ -21,5 +79,16 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     end
     local file = vim.uv.fs_realpath(event.match) or event.match
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-- Format on save using conform (handled via autocmd instead of conform's format_on_save to prevent E13 errors)
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("format_on_save"),
+  callback = function(event)
+    if vim.g.autoformat and vim.bo[event.buf].buftype == "" then
+      local conform = require("conform")
+      conform.format({ bufnr = event.buf, timeout_ms = 500, lsp_fallback = true })
+    end
   end,
 })
--- lua/daniel/options.lua
+++ lua/daniel/options.lua
@@ -1,21 +1,73 @@
 local opt = vim.opt
 
+vim.g.autoformat = true
+vim.g.snacks_animate = true
+vim.g.ai_cmp = true -- Enable AI completions via blink.cmp
+
 opt.clipboard = "unnamedplus"
 
 opt.number = true
-opt.relativenumber = true 
-opt.tabstop = 2 
+opt.relativenumber = true
+opt.tabstop = 2
 opt.shiftwidth = 2 -- Size of an indent
 
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
+-- opt.autowrite = true -- Enable auto write
+-- opt.completeopt = "menu,menuone,noselect"
+-- opt.conceallevel = 2 -- Hide * markup for bold and italic
+opt.confirm = true -- Don't confirm to save changes before exiting modified buffer
 opt.cursorline = true -- Enable highlighting of the current line
 opt.expandtab = true -- Use spaces instead of tabs
 opt.mouse = "a" -- Enable mouse mode
 
-opt.termguicolors = true 
+opt.termguicolors = true -- True color support
 opt.undofile = true
 opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
-opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mod
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
 opt.wrap = false -- Disable line wrap
 
+opt.timeoutlen = 300 -- Lower than default to quickly trigger which-key
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.ruler = false -- Disable the default ruler
+opt.scrolloff = 4 -- Lines of context
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+opt.shiftround = true -- Round indent
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+opt.showmode = false -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8 -- Columns of context
+opt.signcolumn = "yes" -- Always show the signcolumn
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
+opt.smoothscroll = true
+opt.spelllang = { "en" }
+opt.splitbelow = true -- Put new windows below current
+opt.splitkeep = "screen"
+opt.splitright = true -- Put new windows right of current
+
+opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
+}
+opt.foldlevel = 99
+opt.foldmethod = "indent"
+opt.foldtext = ""
+opt.formatexpr = "v:lua.require('conform').formatexpr()" -- Use conform.nvim for formatting
+opt.formatoptions = "jcroqlnt"
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.ignorecase = true -- Ignore case
+opt.inccommand = "nosplit" -- preview incremental substitute
+opt.jumpoptions = "view"
+opt.laststatus = 3 -- global statusline
+opt.linebreak = true -- Wrap lines at convenient points
+opt.list = true -- Show some invisible characters
+opt.winminwidth = 5 -- Minimum window width
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
